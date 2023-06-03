bceaad7be553390affddee4a53a35eb1227a936d
Jun Hayashi <81370008+juny21@users.noreply.github.com>
2023-06-02 22:42:08 -0300

    Update T3_BD2.py

--- T3_BD2.py
+++ T3_BD2.py
@@ -2,127 +2,149 @@ import psycopg2
 import matplotlib.pyplot as plt
 
 # Função para conectar ao banco de dados
-def conectar_bd():
+def connect():
     conn = psycopg2.connect(
-        host="seu_host",
-        database="seu_banco_de_dados",
-        user="seu_usuario",
-        password="sua_senha"
+        host="localhost",
+        database="T2.sql",
+        user="",
+        password=""
     )
     return conn
 
-
 # Função para gerar a programação dos jogos
-def gerar_programacao_jogos():
-    conn = conectar_bd()
-    cur = conn.cursor()
+def gerar_programacao():
+    conn = connect()
+    cursor = conn.cursor()
 
-    cur.execute("""
-        SELECT j.num_jogo, p1.nome AS jogador1, p2.nome AS jogador2, pa.nome AS arbitro, s.nome_hotel, j.dia_jorn, j.mes_jorn, j.ano_jorn
+    # Consulta para obter os detalhes dos jogos
+    query = """
+        SELECT j.num_jogo, j.dia_jorn, j.mes_jorn, j.ano_jorn, p1.nome AS jogador1, p2.nome AS jogador2, p.nome AS arbitro, s.nome_hotel, s.capacidade
         FROM Jogo j
-        JOIN Joga jg1 ON j.num_jogo = jg1.num_jogo
-        JOIN Joga jg2 ON j.num_jogo = jg2.num_jogo
-        JOIN Participante p1 ON jg1.num_jogador = p1.num_participante
-        JOIN Participante p2 ON jg2.num_jogador = p2.num_participante
-        JOIN Participante pa ON j.num_arb = pa.num_participante
-        JOIN Salao s ON j.num_salao = s.num_salao
-    """)
+        INNER JOIN Joga jg1 ON j.num_jogo = jg1.num_jogo
+        INNER JOIN Joga jg2 ON j.num_jogo = jg2.num_jogo
+        INNER JOIN Participante p1 ON jg1.num_jogador = p1.num_participante
+        INNER JOIN Participante p2 ON jg2.num_jogador = p2.num_participante
+        INNER JOIN Participante p ON j.num_arb = p.num_participante
+        INNER JOIN Salao s ON j.num_salao = s.num_salao
+    """
 
-    programacao_jogos = cur.fetchall()
-    cur.close()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    # Exibir os detalhes dos jogos
+    for row in rows:
+        num_jogo, dia_jorn, mes_jorn, ano_jorn, jogador1, jogador2, arbitro, nome_hotel, capacidade = row
+        print(f"Jogo #{num_jogo}")
+        print(f"Data: {dia_jorn}/{mes_jorn}/{ano_jorn}")
+        print(f"Jogadores: {jogador1} vs {jogador2}")
+        print(f"Árbitro: {arbitro}")
+        print(f"Local: {nome_hotel} (Capacidade: {capacidade})")
+        print("----------------------------------------")
+
+    cursor.close()
     conn.close()
 
-    return programacao_jogos
+# Função para gerar a lista de jogos programados para um Hotel, jogador e árbitro específico
+def listar_jogos_programados(hotel, jogador, arbitro):
+    conn = connect()
+    cursor = conn.cursor()
 
-
-# Função para gerar a lista de jogos programados para um hotel, jogador e árbitro específico
-def gerar_jogos_programados(nome_hotel, num_jogador, num_arbitro):
-    conn = conectar_bd()
-    cur = conn.cursor()
-
-    cur.execute("""
-        SELECT j.num_jogo, p1.nome AS jogador1, p2.nome AS jogador2, pa.nome AS arbitro, s.nome_hotel, j.dia_jorn, j.mes_jorn, j.ano_jorn
+    # Consulta para obter os jogos programados
+    query = """
+        SELECT j.num_jogo, j.dia_jorn, j.mes_jorn, j.ano_jorn
         FROM Jogo j
-        JOIN Joga jg1 ON j.num_jogo = jg1.num_jogo
-        JOIN Joga jg2 ON j.num_jogo = jg2.num_jogo
-        JOIN Participante p1 ON jg1.num_jogador = p1.num_participante
-        JOIN Participante p2 ON jg2.num_jogador = p2.num_participante
-        JOIN Participante pa ON j.num_arb = pa.num_participante
-        JOIN Salao s ON j.num_salao = s.num_salao
-        WHERE s.nome_hotel = %s AND jg1.num_jogador = %s AND j.num_arb = %s
-    """, (nome_hotel, num_jogador, num_arbitro))
+        INNER JOIN Salao s ON j.num_salao = s.num_salao
+        INNER JOIN Hotel h ON s.nome_hotel = h.nome_hotel
+        INNER JOIN Joga jg1 ON j.num_jogo = jg1.num_jogo
+        INNER JOIN Joga jg2 ON j.num_jogo = jg2.num_jogo
+        INNER JOIN Participante p1 ON jg1.num_jogador = p1.num_participante
+        INNER JOIN Participante p2 ON jg2.num_jogador = p2.num_participante
+        INNER JOIN Participante p ON j.num_arb = p.num_participante
+        WHERE h.nome_hotel = %s AND p1.nome = %s AND p.nome = %s
+    """
 
-    jogos_programados = cur.fetchall()
-    cur.close()
+    cursor.execute(query, (hotel, jogador, arbitro))
+    rows = cursor.fetchall()
+
+    # Exibir os jogos programados
+    for row in rows:
+        num_jogo, dia_jorn, mes_jorn, ano_jorn = row
+        print(f"Jogo #{num_jogo}")
+        print(f"Data: {dia_jorn}/{mes_jorn}/{ano_jorn}")
+        print("----------------------------------------")
+
+    cursor.close()
     conn.close()
 
-    return jogos_programados
-
-
-# Função para gerar a curva de jogos por número de movimentos
+# Função para gerar uma curva que apresente os jogos por número de movimentos
 def gerar_curva_movimentos():
-    conn = conectar_bd()
-    cur = conn.cursor()
+    conn = connect()
+    cursor = conn.cursor()
 
-    cur.execute("""
+    # Consulta para obter o número de movimentos de cada jogo
+    query = """
         SELECT num_jogo, COUNT(*) AS num_movimentos
         FROM Movimento
         GROUP BY num_jogo
-    """)
+    """
 
-    resultados = cur.fetchall()
-    cur.close()
-    conn.close()
+    cursor.execute(query)
+    rows = cursor.fetchall()
 
-    num_jogos = [r[0] for r in resultados]
-    num_movimentos = [r[1] for r in resultados]
+    # Extrair os dados para o gráfico
+    jogos = []
+    num_movimentos = []
+    for row in rows:
+        num_jogo, num_movimento = row
+        jogos.append(num_jogo)
+        num_movimentos.append(num_movimento)
 
-    # Plotar a curva
-    plt.plot(num_jogos, num_movimentos)
-    plt.xlabel('Número do Jogo')
-    plt.ylabel('Número de Movimentos')
-    plt.title('Curva de Jogos por Número de Movimentos')
+    # Gerar o gráfico
+    plt.plot(jogos, num_movimentos)
+    plt.xlabel("Número do Jogo")
+    plt.ylabel("Número de Movimentos")
+    plt.title("Curva de Jogos por Número de Movimentos")
     plt.show()
 
+    cursor.close()
+    conn.close()
 
-# Função para gerar a curva de número de jogadores por país
+# Função para gerar uma curva que liste o número de jogadores por país
 def gerar_curva_jogadores_por_pais():
-    conn = conectar_bd()
-    cur = conn.cursor()
+    conn = connect()
+    cursor = conn.cursor()
 
-    cur.execute("""
+    # Consulta para obter o número de jogadores por país
+    query = """
         SELECT p.num_pais, COUNT(*) AS num_jogadores
-        FROM Participante p
-        WHERE p.tipo_part = 'jogador'
+        FROM Jogador j
+        INNER JOIN Participante p ON j.num_participante = p.num_participante
         GROUP BY p.num_pais
-    """)
+    """
 
-    resultados = cur.fetchall()
-    cur.close()
-    conn.close()
+    cursor.execute(query)
+    rows = cursor.fetchall()
 
-    paises = [r[0] for r in resultados]
-    num_jogadores = [r[1] for r in resultados]
+    # Extrair os dados para o gráfico
+    paises = []
+    num_jogadores = []
+    for row in rows:
+        num_pais, num_jogador = row
+        paises.append(num_pais)
+        num_jogadores.append(num_jogador)
 
-    # Plotar a curva
+    # Gerar o gráfico
     plt.bar(paises, num_jogadores)
-    plt.xlabel('Número do País')
-    plt.ylabel('Número de Jogadores')
-    plt.title('Curva de Número de Jogadores por País')
+    plt.xlabel("Número do País")
+    plt.ylabel("Número de Jogadores")
+    plt.title("Curva de Número de Jogadores por País")
     plt.show()
 
+    cursor.close()
+    conn.close()
 
-# Exemplos de uso
-programacao_jogos = gerar_programacao_jogos()
-print("Programação de Jogos:")
-for jogo in programacao_jogos:
-    print(jogo)
-
-jogos_programados = gerar_jogos_programados("Nome do Hotel", 1, 2)
-print("Jogos Programados:")
-for jogo in jogos_programados:
-    print(jogo)
-
+# Exemplo de uso das funções
+gerar_programacao()
+listar_jogos_programados("Nome do Hotel", "Nome do Jogador", "Nome do Árbitro")
 gerar_curva_movimentos()
-
 gerar_curva_jogadores_por_pais()
